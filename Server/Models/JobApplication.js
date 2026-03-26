7ed47ed1b709cf38df22a2fe8bdffb619ae4b646
nitesh8652 <niteshsalian8652085233@gmail.com>
2026-03-26 19:15:29 +0530

    Adding drop down

--- Server/Models/JobApplication.js
+++ Server/Models/JobApplication.js
@@ -4,7 +4,7 @@ const JobApplicationSchema = new mongoose.Schema({
      userId:{type:String, ref:'User' ,required:true},
     companyId:{type:mongoose.Schema.Types.ObjectId, ref:'Company' ,required:true},
     jobId:{type:mongoose.Schema.Types.ObjectId, ref:'Job' ,required:true},
-    status:{type:String , default:'applied'},  
+    status:{type:String , default:'Applied'},  
     date:{type:Number, required:true}
 })
 
