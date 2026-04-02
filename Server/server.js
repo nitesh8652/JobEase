f9a32b774a02bd6913480cd428bbc4be54c00c67
nitesh8652 <niteshsalian8652085233@gmail.com>
2026-04-03 04:28:35 +0530

    deployment

--- Server/server.js
+++ Server/server.js
@@ -18,7 +18,7 @@ await connectDB();
 await connectCloudinary()
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+ origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }))
 
